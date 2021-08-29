16646ad0a96400f8f82a80df3c5ebeb53e8dccdc
sickpickle <test@gmail.com>
2021-08-29 11:07:26 +0000

    Week3 AC code upload

---
+++ wjsgnlcks/Week3/14719-sickpickel.cpp
@@ -0,0 +1,37 @@
+// 빗물
+// 쉽게 푸는 법
+#include <bits/stdc++.h>
+#define Max 501 
+
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	
+	int arr[Max][Max];
+	int h, w, input;
+	
+	cin >> h >> w;
+	for(int i = 0; i < w; i++){
+		cin >> input;
+		while(input--) arr[input][i] = 1;
+	}
+	
+	int ans = 0;	
+	for(int i = 0; i < h; i++){
+		int flag = 0, count = 0;
+		for(int j = 0; j < w; j++){
+			if(arr[i][j]) flag = 1;
+			if(!flag) continue;
+			
+			if(arr[i][j]){
+				ans += count;	
+				count = 0;
+			}
+			else count++;
+		}
+	}
+	
+	cout << ans << "\n";	
+	return 0;
+}
---
+++ wjsgnlcks/Week3/2133-sickpickelc.cpp
@@ -0,0 +1,22 @@
+// 타일 채우기
+#include <bits/stdc++.h>
+#define Max 31 
+
+using namespace std;
+
+int shapeA[Max], shapeB[Max], shapeC[Max];
+
+int main(void){
+	int n;
+	cin >> n;
+	shapeA[1] = shapeB[1] = shapeC[1] = 0;	
+	shapeA[2] = shapeB[2] = 1;
+	shapeC[2] = 3;
+	for(int i = 3; i <= n; i++){
+		shapeA[i] = shapeA[i - 2] + shapeC[i - 2];
+		shapeB[i] = shapeB[i - 2] + shapeC[i - 2];
+		shapeC[i] = shapeA[i] + shapeB[i] + shapeC[i - 2];
+	}
+	cout << shapeC[n] << "\n";	
+	return 0;
+}
