a7705edb1b41c0772277ece5f0a1335d6178371b
JWC <jwchoi179@soongsil.ac.kr>
2021-09-06 13:45:40 +0900

    9625 성공

--- jwchoi/5Week/9625.cpp
+++ jwchoi/5Week/9625.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int main(void)
+{
+    int n;
+
+    int A[46], B[46];
+
+    A[0] = 0;
+    A[1] = 1;
+
+    B[0] = 1;
+    B[1] = 1;
+
+    cin >> n;
+
+    if (n >= 2)
+    {
+        for (int i = 2; i < n; ++i)
+        {
+            A[i] = A[i - 2] + A[i - 1];
+            B[i] = B[i - 2] + B[i - 1];
+        }
+    }
+    cout << A[n - 1] << " " << B[n - 1] << endl;
+
+    return 0;
+}
