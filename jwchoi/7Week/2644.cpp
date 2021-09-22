41fc844723a75a2c841c2e397740ceb7fb963e02
JWC <jwchoi179@soongsil.ac.kr>
2021-09-22 23:08:51 +0900

    Sucess 2644 5639

--- jwchoi/7Week/2644.cpp
+++ jwchoi/7Week/2644.cpp
@@ -1,1 +1,51 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+vector<vector<int>> adj;
+vector<bool> visited;
+
+int ans = -1;
+
+void DFS(int p1, int p2, int cnt) {
+  int next;
+
+  visited[p1] = true;
+
+  if (p1 == p2) {
+    ans = cnt;
+  }
+
+  for (int i = 0; i < adj[p1].size(); ++i) {
+    next = adj[p1][i];
+
+    if (!visited[next]) {
+      DFS(next, p2, cnt + 1);
+    }
+  }
+}
+
+int main(void) {
+  int n, p1, p2, m, x, y, cnt = 0;
+
+  cin >> n >> p1 >> p2 >> m;
+
+  adj.resize(n + 1);
+
+  for (int i = 0; i < m; ++i) {
+    cin >> x >> y;
+
+    adj[x].push_back(y);
+    adj[y].push_back(x);
+  }
+
+  visited = vector<bool>(m + 1, false);
+
+  DFS(p1, p2, cnt);
+
+  cout << ans;
+
+  return 0;
+}
--- jwchoi/7Week/5639.cpp
+++ jwchoi/7Week/5639.cpp
@@ -1,1 +1,72 @@
 
+/*
+EOF에 대한 이해가 필요
+
+문제에서 제공하는 예시들만 만족하면 되는 것이 아니라, 임의의 전위 순회
+결과값들을 전부 입력했을 때, 모든 경우에 해당이 되어야 함
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  Node *left = NULL;
+  Node *right = NULL;
+};
+
+Node *newnode(Node *node, int val) {
+  if (node == NULL) {
+    node = new Node();
+    node->data = val;
+    node->left = node->right = NULL;
+
+    /*
+    node->left = NULL;
+    node->right = NULL;
+    동일한 연산
+    */
+  } else if (val <= node->data)
+    node->left = newnode(node->left, val);
+  else
+    node->right = newnode(node->right, val);
+
+  return node;
+}
+
+void PostOrderTravserse(Node *pnode) {
+  if (pnode->left) {
+    PostOrderTravserse(pnode->left);
+  }
+
+  if (pnode->right) {
+    PostOrderTravserse(pnode->right);
+  }
+
+  cout << pnode->data << endl;
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int val;
+  Node *rnode = NULL;
+
+  while (true) {
+    cin >> val;
+
+    if (cin.eof())
+      break;
+
+    rnode = newnode(rnode, val);
+  }
+
+  PostOrderTravserse(rnode);
+
+  return 0;
+}
